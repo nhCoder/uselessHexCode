1520c0ceecb14717468a170a3e61ad6187781d0a
Naveed Hassan <naveedhassan913@gmail.com>
2019-09-16 06:05:52 +0500

    GMT

--- skynotify.js
+++ skynotify.js
@@ -1,1 +1,1 @@
-Follow me on <a href="https://www.instagram.com/naveedhassan913/"> instagram</a> .Please don't enter others userId, use yourself.Otherwise your ip will be banned
+Follow me on <a href="https://www.instagram.com/naveedhassan913/"> instagram</a> .Please don't enter others userId, use your own

dd55d906bc824c206c60ad75a9d745c7cb3317d1
Naveed Hassan <naveedhassan913@gmail.com>
2019-09-16 07:03:23 +0500

    Find

--- skynotify.js
+++ skynotify.js
@@ -1,1 +1,2 @@
-Follow me on <a href="https://www.instagram.com/naveedhassan913/"> instagram</a> .Please don't enter others userId, use your own
+Follow me on <a href="https://www.instagram.com/naveedhassan913/"> instagram</a> .Please don't enter others userId, use your own.
+Currently WhatsApp Group is full Don't message me.

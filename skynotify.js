53405454cab90be941e851f324c393f1be360485
Naveed Hassan <naveedhassan913@gmail.com>
2019-09-17 23:25:27 +0500

    Hong

--- skynotify.js
+++ skynotify.js
@@ -1,3 +1,5 @@
 New <b>Telegram</b> Group join kare  
-<a href="https://t.me/joinchat/NwZN4RUWw5pivBpqAJ2bjA"> click</a> 
-Whatsapp group nai chal raha
+<a href="https://t.me/joinchat/NwZN4RUWw5pivBpqAJ2bjA">click</a> 
+.
+For Premium account contact me on
+<a href="https://www.instagram.com/naveedhassan913/"> instagram</a>

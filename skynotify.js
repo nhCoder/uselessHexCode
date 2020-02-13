883ed21f69b86b423d8d6e9cef3caf9191aa1509
Naveed Hassan <naveedhassan913@gmail.com>
2020-02-14 02:48:03 +0500

    V

--- skynotify.js
+++
@@ -1,5 +0,0 @@
-New <b>Telegram</b> Group join kare  
-<a href="https://t.me/joinchat/NwZN4RUWw5pivBpqAJ2bjA">click</a> 
-.
-For Premium account contact me on
-<a href="https://www.instagram.com/naveedhassan913/"> instagram</a>
